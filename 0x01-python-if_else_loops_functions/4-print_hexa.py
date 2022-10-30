6991bf791eb31ab5af7c03601c79f7a67bd2d8f2
Ivordean <ivordean1@gmail.com>
2022-10-30 04:37:48 -0700

    my python program

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+i#!/usr/bin/python3
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))i
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This is my README.md file for the 0x01 Python if else loops and functions project
